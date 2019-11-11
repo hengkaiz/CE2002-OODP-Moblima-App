ca1bf4c8655a2d695f452ac94063c2ca6b1b6876
yixingchin <57394787+yixingchin@users.noreply.github.com>
2019-11-11 19:25:10 +0800

    yixing

--- Code/src/user/ChooseSeats.java
+++ Code/src/user/ChooseSeats.java
@@ -1,5 +1,47 @@
 package user;
 
-public class ChooseSeats {
 
+import java.util.Scanner;
+
+public class ChooseSeats {
+	
+	public int[][] seats = new int[7][7];
+	public boolean taken;
+	
+	public void initializeSeats() {
+		for (int i =0; i <seats.length; ++i) {
+			for (int j =0; j<seats[i].length; ++j) {
+				seats[i][j] = 'O';
+			}
+		}
+	}
+	
+	
+	public void displaySeats() {
+		for (int i =0; i <seats.length; ++i) {
+			for (int j =0; j<seats[i].length; ++j) {
+				System.out.println(seats[i][j]);
+			}
+		}
+	}
+	
+	public void ChooseSeats() {
+		System.out.println("The seats marked 'O' are empty, 'X' are occupied");
+		System.out.println("---------SCREEN----------");
+		displaySeats();
+		System.out.println("Please enter the row of desired seat");
+		Scanner sc = new Scanner(System.in);
+		int selectRow = sc.nextInt();
+		System.out.println("Please enter the column of desired seat");
+		int selectColumn = sc.nextInt();
+		if (this.seats[selectRow][selectColumn] != 'X') {
+			this.seats[selectRow][selectColumn] = 'X';
+		}
+		else {
+			System.out.println("Sorry seat has already been taken");
+		}
+		
+	}
+	
+	
 }
