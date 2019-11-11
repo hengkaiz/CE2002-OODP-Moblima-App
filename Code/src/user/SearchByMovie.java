f4449b137bd9577bb23d869b2a7435e7d0c65c58
yixingchin <57394787+yixingchin@users.noreply.github.com>
2019-11-11 20:00:31 +0800

    yixing

--- Code/src/user/SearchByMovie.java
+++ Code/src/user/SearchByMovie.java
@@ -1,5 +1,17 @@
 package user;
+import Movie.Movie;
+import java.util.ArrayList;
 
-public class SearchByMovie {
+public class SearchByMovie implements SearchFunctions{
+	private Movie movie;
+
+	
+	public void SearchResults(String movie) {
+		for (int i=0; i<len(Movie); i++) {
+			if (movie = Movie[i].title) {
+				
+			}
+		}
+	}
 
 }
