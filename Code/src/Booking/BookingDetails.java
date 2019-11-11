220ee3a86756a05b95cc9d3d3ac7c98ecfdec476
Hope <Hope@10.27.172.254>
2019-11-11 20:00:59 +0800

    i tried

--- Code/src/Booking/BookingDetails.java
+++ Code/src/Booking/BookingDetails.java
@@ -1,5 +1,42 @@
 package Booking;
 
 public class BookingDetails {
+	private String moviegoerName; 
+	private int mobileNumber;
+	private String emailAddress;
+	private int transactionID;
+	private int showtime;
+	private int seatNumber;
+	private double ticketPrice;
+	
+	public BookingDetails(String name, int mobile, String email, int ID, int st, int seatno, double price) {
+		moviegoerName = name;
+		mobileNumber = mobile;
+		emailAddress = email;
+		transactionID = ID;
+		showtime = st;
+		seatNumber = seatno;
+		ticketPrice = price;
+	}
+	
+	public String getmoviegoerName() {
+		return moviegoerName;
+	}
+	
+	public int getmobileNumber() {
+		return mobileNumber;
+	}
+	
+	public String getemailAddress() {
+		return emailAddress;
+	}
+	
+	public int getshowtime() {
+		return showtime;
+	}
+	
+	public double getticketPrice() {
+		return ticketPrice;
+	}
 
 }
