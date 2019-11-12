644023b6e053ea7d0fe3d653b9bb5180d8859ec4
yixingchin <57394787+yixingchin@users.noreply.github.com>
2019-11-12 15:39:52 +0800

    yixing

--- Code/src/user/SearchByDate.java
+++ Code/src/user/SearchByDate.java
@@ -1,10 +1,33 @@
 package user;
-import Showtime;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class SearchByDate {
-	private String date;
+import Showtime;
+import Database
+
+public class SearchByDate implements SearchFunction {
+	private int date;
 	private String movie;
 	
 	
-	public 
+	public void SearchResults(int date) {
+		//creates object dateList
+		ArrayList<Database> dateList = new ArrayList<>();
+		
+		//prints out date list
+		for(int i = 0; i < dateList.size(); i++) {
+            System.out.println(i+1, ".", dateList.get(i).getDate());
+		} 
+        
+        System.out.println("Please select dates based on index");
+        Scanner sc = new Scanner(System.in);
+        int sel = sc.nextInt();
+        
+        //set movieSelected as the input user selected. *need to change movie 
+        int dateSelected =  Database.get(sel-1).getDate();
+        
+        //searches through BookingDatabase, compareTo returns timing
+        Database.searchByDate(dateSelected);
+                
+	}
 }
--- Code/src/user/SearchByMovie.java
+++ Code/src/user/SearchByMovie.java
@@ -1,6 +1,8 @@
 package user;
 import Movie.Movie;
 import java.util.ArrayList;
+import java.util.Scanner;
+import Database;
 
 public class SearchByMovie implements SearchFunctions{
 	private Movie movie;
@@ -8,15 +10,24 @@ public class SearchByMovie implements SearchFunctions{
 	
 	public void SearchResults(String movie) {
 		//creates object movieList
-		List<Movie> movieList = new ArrayList<>();
+		ArrayList<Database> movieList = new ArrayList<>();
 		
 		// prints out movie list
         for(int i = 0; i < movieList.size(); i++) {
-            System.out.println(movieList.get(i).getMovie());
-		}  
-		
-				
-		}
+            System.out.println(i+1, ".", movieList.get(i).getMovie());
+		} 
+        
+        System.out.println("Please select movie based on index");
+        Scanner sc = new Scanner(System.in);
+        int sel = sc.nextInt();
+        
+        //set movieSelected as the input user selected
+        String movieSelected =  Database.get(sel-1).getMovie();
+        
+        //searches through BookingDatabase, compareTo returns timing
+        Database.searchByMovie(movieSelected);
+        
+       }
 		
 	
 
