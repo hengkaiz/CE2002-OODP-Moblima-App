c650c650f90ee3a5dbfed9c162420f4d44ea2d02
jordanchong <jordanchong@10.27.78.161>
2019-11-12 19:33:05 +0800

    m

--- Code/src/Movies/Review.java
+++ Code/src/Movies/Review.java
@@ -7,21 +7,29 @@ public class Review {
 	private double rating;
 	//date
 	private String reviewContent;
-
-	public Review createReview() {
+	
+	public Review() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your name: ");
 		String reviewer=sc.nextLine();
-		this.reviewer = reviewer;
 		
 		System.out.println("Enter a rating out of 10: ");
 		double rating=sc.nextDouble();
-		this.rating = rating;
 		
 		System.out.println("Enter your review: ");
 		String reviewContent=sc.nextLine();
+		
+		this.reviewer = reviewer;
+		this.rating = rating;
+		this.reviewContent = reviewContent;	
+	}
+
+	public Review(String reviewer, double rating, String reviewContent) {
+		this.reviewer = reviewer;
+		this.rating = rating;
 		this.reviewContent = reviewContent;
 	}
+
 	
 	public String getReviewer() {
 		return reviewer;
