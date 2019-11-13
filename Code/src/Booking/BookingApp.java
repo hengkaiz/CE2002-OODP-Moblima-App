9414997ddeb7faeb38e6c3f82df2ffdb0cabe19c
hengkaiz <57538939+hengkaiz@users.noreply.github.com>
2019-11-13 19:32:05 +0800

    fuck moblima login done till booking part

--- Code/src/Booking/BookingApp.java
+++ Code/src/Booking/BookingApp.java
@@ -3,7 +3,7 @@ package Booking;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class BookingApp {
+public class BookingApp{
 	public static void main(String[] args) {
 		BookingDatabase bd = new BookingDatabase();
 		Scanner sc = new Scanner(System.in);
