ccc8029b7378778739fded3527a00b978a961edf
jordanchong <jordanchong@10.27.154.150>
2019-11-14 16:09:51 +0800

    ..

--- Code/src/movies/MovieDatabase.java
+++ Code/src/movies/MovieDatabase.java
@@ -105,7 +105,7 @@ public class MovieDatabase {
 		movieList.sort(new TotalSalesSorter());
 		return movieList.subList(0, num);
 	}
-	
+	//comment
 	
 //	public static void main(String[] args) {
 //		MovieDatabase mdb = new MovieDatabase();
