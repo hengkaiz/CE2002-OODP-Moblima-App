6abcd79f74b588e9be54fbe2a7b56ed0d7d4c2cb
Munnie <Munnie@DESKTOP-RSVS02R>
2019-11-16 03:25:53 +0800

    admin movie menu, editor

--- Code/src/admin/AdminMovieEditor.java
+++ Code/src/admin/AdminMovieEditor.java
@@ -1,37 +1,59 @@
 package admin;
 
 import java.util.Scanner;
-import movies.*;
 import java.util.ArrayList;
-import java.lang.Object;
 
-public class MovieListingEditor {
+import moblima.SaveAndLoadDB;
+import movies.*;
+
+public class AdminMovieEditor {
+	private SaveAndLoadDB saveAndLoadDB = new SaveAndLoadDB();
+	private int cineplexNumber;
+	private MovieDatabase mdb;
+	private Movie selectedMovie;
 	
-	Scanner sc = new Scanner(System.in);
-	MovieDatabase movieDatabase = new MovieDatabase();
-	ArrayList<Movie> mdb = movieDatabase.getMovies();
-	Movie selectedMovie = null;
+	public AdminMovieEditor(int cineplexNumber) {
+		this.cineplexNumber = cineplexNumber;
+		this.mdb = saveAndLoadDB.loadMovieDB();
+	}
 	
 	public void createMovieListing() { //creating a new Movie object
-		movieDatabase.addMovieToDB();
+		mdb.addMovieToDB();
+		saveAndLoadDB.saveMovieDB(mdb);
 	}
 	public void updateMovieListing() {
+		Scanner sc = new Scanner(System.in);
+		int i=0;
+		
+		ArrayList<Movie> movieList = mdb.getMovies();
+		
 		//getting movie to update
 		int updateMovieChoice = 0;
-		System.out.println("Current movies: ");
-		int i=0;
-		for(i=0; i<mdb.size(); i++) {
-			System.out.println((i+1) + ". " + mdb.get(i).getMovieTitle());
+		System.out.println("Current movies in the database: ");
+		i=1;
+		for(Movie m : movieList) {
+			System.out.println(i + ". " + m.getMovieTitle());
 		}
-		System.out.println("Which Movie do you want to update? (-1 to exit)");
-		updateMovieChoice = sc.nextInt();
-		if(updateMovieChoice == -1) {
-			return;
+		boolean loop = true;
+        do {
+        try {
+    		System.out.println("Which Movie do you want to update? (-1 to exit)");
+    		//need exception in case they enter the movie string instead?
+    		updateMovieChoice = sc.nextInt();
+    		if(updateMovieChoice == -1) {
+    			return;
+    		}
+    		if (updateMovieChoice<1 || updateMovieChoice>i) { //check exceptions?
+				throw new Exception();
+			}
+			loop = false;
+		} catch (Exception e) {
+			System.out.printf("Invalid movie. Try Again.");
 		}
-		selectedMovie = mdb.get(updateMovieChoice-1);
+        } while (loop);
+
+		selectedMovie = movieList.get(updateMovieChoice-1);
 		
-		//selection of update
-		//if(checkMovie(selectedMovie)) { //valid movie entry
 		int updateChoice = 0;
 		String exit = "-1";
 		do {
@@ -42,12 +64,25 @@ public class MovieListingEditor {
 			System.out.println("3. Update Movie Director");
 			System.out.println("4. Update Movie Overall Rating");
 			System.out.println("5. Update Movie Status");
-			System.out.println("6. Exit");
+			System.out.println("6. Return");
+			loop = true;
+	        do {
+	        try {
+	    		System.out.println("Please enter selection:"); 
+	    		//need exception in case they enter the movie string instead?
+	    		updateChoice = sc.nextInt();
+				if (updateChoice<1 || updateChoice>6) { //check exceptions?
+					throw new Exception();
+				}
+				loop = false;
+			} catch (Exception e) {
+				System.out.printf("Invalid choice. Try Again.");
+			}
+	        } while (loop);
 			
-			updateChoice = sc.nextInt();
 			switch(updateChoice) {
 			case 1: //update movie synopsis
-				sc.nextLine(); //dummy
+				sc.nextLine(); //clear buffer
 				System.out.println("Enter new Movie Synopsis: ");
 				selectedMovie.setMovieSynopsis(sc.nextLine());
 				System.out.println(selectedMovie.getMovieTitle() + " Synopsis updated");
@@ -60,9 +95,22 @@ public class MovieListingEditor {
 				System.out.println(selectedMovie.toStringMovieCast()); //prints the current cast list
 				System.out.println("1. Add Cast Member");
 				System.out.println("2. Remove Cast Member");
-				System.out.println("3. Exit");
+				System.out.println("3. Return");
+				loop = true;
+		        do {
+		        try {
+		    		System.out.println("Please enter selection:"); 
+		    		//need exception in case they enter the movie string instead?
+		    		castChoice = sc.nextInt();
+					if (castChoice<1 || castChoice>3) { //check exceptions?
+						throw new Exception();
+					}
+					loop = false;
+				} catch (Exception e) {
+					System.out.printf("Invalid choice. Try Again.");
+				}
+		        } while (loop);
 				
-				castChoice = sc.nextInt();
 				switch(castChoice) {
 				case 1: //add cast member
 					sc.nextLine(); //dummy
@@ -100,8 +148,22 @@ public class MovieListingEditor {
 				System.out.println(selectedMovie.toStringMovieDirector()); //prints the current director list
 				System.out.println("1. Add Director");
 				System.out.println("2. Remove Director");
+				System.out.println("3. Return");
+				loop = true;
+		        do {
+		        try {
+		    		System.out.println("Please enter selection:"); 
+		    		//need exception in case they enter the movie string instead?
+		    		directorChoice = sc.nextInt();
+					if (directorChoice<1 || directorChoice>3) { //check exceptions?
+						throw new Exception();
+					}
+					loop = false;
+				} catch (Exception e) {
+					System.out.printf("Invalid choice. Try Again.");
+				}
+		        } while (loop);
 				
-				directorChoice = sc.nextInt();
 				switch(directorChoice) {
 				case 1: //add director
 					sc.nextLine(); //dummy
@@ -157,51 +219,77 @@ public class MovieListingEditor {
 				break;
 			}
 		} while(updateChoice != 6);
-
-		/*else { //invalid movie entry
-			System.out.println("Movie not available!");
-			return;
-		}*/
+		saveAndLoadDB.saveMovieDB(mdb);
 	}
 	public void removeMovieListing() { //remove by setting status to "END_OFSHOWING"		
-		ArrayList<String> mtdb = movieDatabase.getMovieTitlesList();
+		Scanner sc = new Scanner(System.in);
+		int i=0;
+		
+		ArrayList<String> movieTitles = mdb.getMovieTitlesList();
+		ArrayList<Movie> movieList = mdb.getMovies();
+		
+		int choice = 0;
 		System.out.println("Do you want to delete from database or remove from user view?");
 		System.out.println("1. Delete from database ");
 		System.out.println("2. Remove from user view (marking movie as \"End of Showing\")");
+		System.out.println("3. Return");
+		boolean loop = true;
+        do {
+        try {
+    		System.out.println("Please enter selection:"); 
+    		//need exception in case they enter the movie string instead?
+    		choice = sc.nextInt();
+			if (choice<1 || choice>3) { //check exceptions?
+				throw new Exception();
+			}
+			loop = false;
+		} catch (Exception e) {
+			System.out.printf("Invalid choice. Try Again.");
+		}
+        } while (loop);
 		
-		int choice = sc.nextInt();
 		if(choice == 1) { //remove from database
-			movieDatabase.removeMovieFromDB();
+			mdb.removeMovieFromDB();
 			System.out.println("Movie deleted from database");
 		}
 		if(choice == 2) {
+			int updateMovieChoice = 0;
 			//choosing movie to remove
 			System.out.println("Movies currently in user view:");
-			int i=0;
-			for(i=0; i<mtdb.size(); i++) {
-				System.out.println((i+1) + ". " + mtdb.get(i));
+			i=1;
+			for(String title : movieTitles) {
+				System.out.println(i + ". " + title);
+				i++;
 			}
 			System.out.println("Which Movie do you want to remove?");
-			int updateMovieChoice = sc.nextInt();
-			String title = mtdb.get(updateMovieChoice-1);
-			for(i=0; i<mdb.size(); i++) {
-				if(title.equals(mdb.get(i).getMovieTitle())) {
-					selectedMovie = mdb.get(i);
+			loop = true;
+	        do {
+	        try {
+	    		System.out.println("Please enter selection:"); 
+	    		//need exception in case they enter the movie string instead?
+	    		updateMovieChoice = sc.nextInt();
+				if (updateMovieChoice<1 || updateMovieChoice>i) { //check exceptions?
+					throw new Exception();
+				}
+				loop = false;
+			} catch (Exception e) {
+				System.out.printf("Invalid choice. Try Again.");
+			}
+	        } while (loop);
+			
+			String title = movieTitles.get(updateMovieChoice-1);
+			for(Movie m : movieList) {
+				if(title.equals(m.getMovieTitle())) {
+					selectedMovie = m;
 				}
 			}
-			
 			//set status of selectedMovie to "END_OF_SHOWING"
 			selectedMovie.setStatus(MovieStatus.END_OF_SHOWING);
 			System.out.println("Movie removed from user view");
 		}
-
-	}
-	public boolean checkMovie(Movie m) {
-		for(int i=0; i < mdb.size(); i++) {
-			if(selectedMovie.equals(mdb.get(i))) {
-				return true;
-			}
+		if(choice == 3) {
+			return;
 		}
-		return false;
+		saveAndLoadDB.saveMovieDB(mdb);
 	}
 }
---
+++ Code/src/admin/AdminMovieEditorMenu.java
@@ -0,0 +1,54 @@
+package admin;
+
+import java.util.Scanner;
+import moblima.MenuTemplate;
+
+public class AdminMovieEditorMenu extends MenuTemplate{
+	private MenuTemplate nextMenu;
+	
+	public AdminMovieEditorMenu(MenuTemplate previousMenu) {
+		super(previousMenu);
+	}
+
+	public MenuTemplate run() {
+		Scanner sc = new Scanner(System.in);
+		int sel=0;
+		
+		System.out.println("---Edit Movies---");
+		System.out.println("1. Create Movies");
+		System.out.println("2. Update Movies");
+		System.out.println("3. Remove Movies");
+		System.out.println("4. Return");
+		boolean loop = true;
+		do {
+			try {
+				sel = sc.nextInt();
+				if (sel < 0 || sel > 4) { //check exceptions?
+					throw new Exception();
+				}
+				loop = false;
+			} catch (Exception e) {
+				System.out.println("Invalid Choice. Try Again.");
+			}
+		} while (loop);
+		nextMenu = this;
+		AdminMovieEditor mEditor = new AdminMovieEditor(super.getCineplexNum());
+				
+		switch(sel) {
+		case 1: //create
+			mEditor.createMovieListing();
+			break;
+		case 2: //update
+			mEditor.updateMovieListing();
+			break;
+		case 3: //remove
+			mEditor.removeMovieListing();
+			break;
+		case 4: //return
+			super.returnPrevious();
+			break;
+			
+		}
+		return nextMenu.run();
+	}
+}
