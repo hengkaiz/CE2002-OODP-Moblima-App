1e28be2c364556ba87ca60ad0c46717d7c71d814
yixingchin <57394787+yixingchin@users.noreply.github.com>
2019-11-16 20:31:12 +0800

    exception

--- Code/src/moblima/MainMenu.java
+++ Code/src/moblima/MainMenu.java
@@ -23,18 +23,17 @@ public class MainMenu extends MenuTemplate {
         System.out.println("2. Admin");
         System.out.println("3. Exit");
         System.out.print("Please enter your choice: ");
-        boolean loop = true;
-        do {
+
         try {
 			sel = sc.nextInt();
 			if (sel<1 | sel >3) {
 				throw new Exception();
 			}
-			loop = false;
+
 		} catch (Exception e) {
 			System.out.println("Invalid Selection. Try again.");
 		}
-        } while (loop);
+
         System.out.println();
         sc.nextLine(); //clear the buffer
 
