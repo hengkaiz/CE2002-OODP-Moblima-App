64a910736adda295b2cf54dbf46a422878c925a4
yixingchin <57394787+yixingchin@users.noreply.github.com>
2019-11-17 14:33:36 +0800

    javadocs

--- Code/src/cinema/Cinema.java
+++ Code/src/cinema/Cinema.java
@@ -11,11 +11,23 @@ public class Cinema implements Serializable{
     private int cinemaNumber;
     private String ofCineplexCode;
     private String ofCineplexName;
+    
+    /**
+     * This construct a cinema object for cinema class, using the CinemaType and cinemaNumber as parameters.
+     * @param type input for the type of cinema
+     * @param cinemaNumber input for the cinema number associated with cinema
+     */
 
     public Cinema(CinemaType type, int cinemaNumber){
         this.type = type;
         this.cinemaNumber = cinemaNumber;
     }
+    
+    /**
+     * This construct a cinema object for cinema class, using the ofCineplexCode(Cineplex's Code) and ofCineplexName (Cineplex's Name) as parameters.
+     * @param ofCineplexCode input for code of the Cineplex the cinema is in
+     * @param ofCineplexName input for name of Cineplex the cinema is in
+     */
     public Cinema(String ofCineplexCode, String ofCineplexName) {
     	Scanner sc = new Scanner(System.in);
     	System.out.println("Enter cinema number");
@@ -46,29 +58,57 @@ public class Cinema implements Serializable{
     	this.ofCineplexCode = ofCineplexCode;
     	this.ofCineplexName = ofCineplexName;
     }
-
+	/**
+	 * 
+	 * @return current type
+	 */
     public String getType(){
         return type.getName();
     }
-
+    /**
+     * 
+     * @param type type to set
+     */
     public void setType(CinemaType type) {
         this.type = type;
     }
-
+    /**
+     * 
+     * @return current code
+     */
     public String getCode() {
         return code;
     }
-
+    /**
+     * 
+     * @param code code to set
+     */
     public void setCode(String code) {
         this.code = code;
     }
+    /**
+     * 
+     * @return current Cinema Number
+     */
 	public int getCinemaNumber() {
 		return cinemaNumber;
 	}
+	/**
+	 * 
+	 * @param num Cinema Number to set
+	 */
 	public void setCinemaNumber(int num){this.cinemaNumber = num;}
+	/**
+	 * 
+	 * @return current cineplex's code
+	 */
 	public String getOfCineplexCode() {
 		return ofCineplexCode;
 	}
+	/**
+	 * 
+	 * @return current cineplex's name
+	 */
 	public String getOfCineplexName() {
 		return ofCineplexName;
 	}
